43a791f083bf12afe1a33911006c2ebc6a3a283b
Bilal Bjo <bilalbjo@Bilals-MacBook-Pro.local>
2019-09-05 19:26:40 +0200

    Updates

---
+++ precache-manifest.1cf466bd3d27fefca49bec3f658b62ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6dc3d0be0ce391187aa73baac7ba4e",
+    "url": "/groupTraining/index.html"
+  },
+  {
+    "revision": "1d9e322e2efed089ee52",
+    "url": "/groupTraining/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a730d5c2f1f8fd2b755",
+    "url": "/groupTraining/static/js/2.65e324cd.chunk.js"
+  },
+  {
+    "revision": "1d9e322e2efed089ee52",
+    "url": "/groupTraining/static/js/main.555ac9f4.chunk.js"
+  },
+  {
+    "revision": "48c6ec6199f4f02a6ce4",
+    "url": "/groupTraining/static/js/runtime~main.936e34b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/groupTraining/static/media/logo.5d5d9eef.svg"
+  }
+]);
